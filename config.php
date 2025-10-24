fa88765da64a557001364c5ef260f4e04a664bb9
Puneeth4796 <117646093+Puneeth4796@users.noreply.github.com>
2025-10-24 22:27:37 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 // Secret configuration
-define('word1', REDACTED_SECRET);
-define('word2', REDACTED_SECRET);
+define('word1', ae6a8c050f1a33dc4ebdfc43403137f9d13d0f0e);
+define('word2', B/hfnar+nLTlvEoK/glSafcu5g83u1qqY7Y1GYDp);
 ?>
