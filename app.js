45ade6b57631de50ffd8b74be277ce37e4c14779
Puneeth4796 <117646093+Puneeth4796@users.noreply.github.com>
2025-10-28 13:00:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-// Secret keys for application
-const word1 =REDACTED_SECRET
-constREDACTED_SECRET
+const word1 = "1e58391f085edefad3f4624d724618ba09621fd90d281e3a1ee5d31afa7d02f5";
+const word2 = "REDACTED_SECRET";
 
 // Example usage
 console.log("word1:", word1);
